8d1e247814865d6ed8399fefa4c2ab0bad225481
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-08-29 14:06:08 +0200

    add model, dto, mapper, change server port

---
+++ src/main/java/pl/iwona/designcar/mapper/Utils.java
@@ -0,0 +1,32 @@
+package pl.iwona.designcar.mapper;
+
+import java.lang.reflect.InvocationTargetException;
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.springframework.stereotype.Component;
+import pl.iwona.designcar.dto.CarDto;
+import pl.iwona.designcar.model.Car;
+
+
+@Component
+public class Utils {
+
+    public Object mapperFromDtoToEntity(Object obj) throws IllegalAccessException, InvocationTargetException {
+        if (obj instanceof CarDto) {
+            Car car = new Car();
+            BeanUtilsBean.getInstance().copyProperties(car, obj);
+            return car;
+        } else {
+            throw new IllegalAccessException();
+        }
+    }
+
+    public Object mapperFromEntityToDto(Object obj) throws InvocationTargetException, IllegalAccessException {
+        if (obj instanceof Car) {
+            CarDto carDto = new CarDto();
+            BeanUtilsBean.getInstance().copyProperties(carDto, obj);
+            return carDto;
+        } else {
+            throw new IllegalAccessException();
+        }
+    }
+}
