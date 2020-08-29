a99a6818a70e47fb063a23da751b949dbdc8e33d
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-08-29 14:08:39 +0200

    add repository

---
+++ src/main/java/pl/iwona/designcar/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package pl.iwona.designcar.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.iwona.designcar.model.Role;
+import pl.iwona.designcar.model.RoleName;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+}
