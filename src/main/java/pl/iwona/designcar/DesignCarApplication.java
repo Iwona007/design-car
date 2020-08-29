1e7cee83bcf02e2171d73598cd3994508e1e5b43
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-08-29 14:00:56 +0200

    initail commit

---
+++ src/main/java/pl/iwona/designcar/DesignCarApplication.java
@@ -0,0 +1,13 @@
+package pl.iwona.designcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/iwona/designcar/DesignCarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iwona.designcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
